4f54a75bceb0ccb30b8b6dc2fce41e1757bcab29
Aman Jhunjhunwala <amanjhunjhunwala@outlook.com>
2015-09-25 13:11:39 +0530

    Adding Comments

--- astropython/main/views.py
+++ astropython/main/views.py
@@ -1,3 +1,8 @@
+"""
+This file handles all the abckground computation that happens between calling any URL and obtaining a Template Response
+
+Note : Context refers to the items we wish to pass to our templates.
+"""
 from django.shortcuts import render,HttpResponseRedirect,Http404,RequestContext
 from django.contrib.auth import logout
 from django.core.paginator import Paginator
@@ -19,49 +24,66 @@ from .models import *
 from .utilities import *
 from taggit.models import Tag,TaggedItem
 
+"""
+Generates our landing page / homepage....
+Only those slider images are fetched from the Database that fall in the specified date range
+(Each image has a date range between which it will be displayed on the website
+Images without date range are displayed forever !)
+After fetching the images it shuffles them , so everytime a different image appears on the homepage
+
+This is followed by fetching latest posts from Teach & Learn, Forum and Packages Section and passing everything to the templates
+
+"""
 def home(request):
     sp=[]
     slider_list=Slider.objects.filter(Q(start_date_time__lte=datetime.now()),(Q(end_date_time__gte=datetime.now())|Q(end_date_time__isnull=True))).all()
     for s in slider_list:
         sp.append(s)
-    kk=sorted(sp, key=lambda k: random.random())
-    sp=kk
+    shuffle_images=sorted(sp, key=lambda k: random.random()) #Shuffle with a random key
+    sp=shuffle_images
     context = {'g_message':'< A Google Summer of Code, 2015 Creation />','sliders':sp,'tl_posts':get_all_objects("tl")[:8],'forum_posts':get_all_objects("forum")[:8],'package_posts':Package.objects.filter(state="submitted").order_by('-published').all()[:4]}
     return render(request, 'index.html', context)
 
+"""
+View that logs out a logged in user
+"""
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
 
+"""
+View that utilizes the creation form to add new articles to the website
+"""
 def create(request,section,**kwargs):
-    model=get_model(section)
-    name =get_name(section)
-    tags=[]
+    model=get_model(section)#Identify the type of article - Blogs,Tutorials,etc
+    name =get_name(section) #Fetch generic name
+    tags=[]#List to store all tags that are currently associated with the article type
     t=model.tags.all()
     for tag in t:
         tags.append(tag.name)
-    tags= sorted(tags, key=lambda s: s.lower())
-    exclude_fields = get_exclude_fields(model)
-    form = get_create_form(request,exclude_fields,model,kwargs)
-    if request.method=="POST":
-        state=set_state(request,form)
-        if form.is_valid():
-            instance=form.save(commit=False)
-            slug = get_slug(request,model,instance.title,kwargs)
-            user=get_user(request)
-            instance.state=state
-            instance.slug=slug
-            instance.save()
-            instance.authors.add(user)
+    tags= sorted(tags, key=lambda s: s.lower())#Sort tags in alphabetical order
+    exclude_fields = get_exclude_fields(model)#Hide secret fields
+    form = get_create_form(request,exclude_fields,model,kwargs)#Fetch form
+    if request.method=="POST":#If user has POSTed anything on our server
+        state=set_state(request,form)#Raw State if user has not pressed "Submit" yet !
+        if form.is_valid():#If form is valid
+            instance=form.save(commit=False)#save form data in a model object, but do not commit to DB
+            slug = get_slug(request,model,instance.title,kwargs)#Get Slug
+            user=get_user(request) #Get author
+            instance.state=state #Store State - "Raw" (saved, not submitted) or "Submitted"
+            instance.slug=slug #Store slug
+            instance.save() #Commit instance to DB
+            instance.authors.add(user) #Add authors
             form.save_m2m()
-            automoderate(instance,user)
+            automoderate(instance,user) # Add to Moderation
+            #Send email to moderators
             try:
                 if instance.state=="submitted":
                     subject="New AstroPython Post !"
                     message="A new post to AstroPython"
                     html_m='<hr /><h2 style="text-align:center"><code><tt><span style="font-family:trebuchet ms,helvetica,sans-serif"><strong>AstroPython - Python for Astronomers</strong></span></tt></code></h2><hr /><p>A Post "'+instance.title+'"&nbsp;&nbsp;has&nbsp;been added. It is waiting for moderation !</p><p>Access it <a href="http://www.astropython.org'+instance.get_absolute_url()+'">here</a> !</p><p>Thank you,</p><p><strong>AstroPython Team</strong></p><hr /><p style="text-align:right"><span style="font-size:10px">Currently , you cannot unsubscribe to these emails</span></p>'
                     from_email="notifications@astropython.org"
-                    send_mail(subject,message,from_email,["amanjjw@gmail.com"], fail_silently=False,html_message=html_m)
+                    send_mail(subject,message,from_email,["amanjjw@gmail.com","taldcroft@gmail.com"], fail_silently=False,html_message=html_m)
             except:
                 print "No Email Provided"
             return render(request,'complete.html',{'section':section,'slug':slug,'state':state,'name':name})
@@ -71,45 +93,47 @@ def create(request,section,**kwargs):
 To view a single model instance
 """
 def single(request,section,slug,**kwargs):
-    model=get_model(section)
-    name=get_name(section)
-    tags=model.tags.all()
-    obj=model.objects.get(slug=slug)
-    mode="display"
+    model=get_model(section)#Identify the type of article - Blogs,Tutorials,etc
+    name=get_name(section) # Get generic name
+    tags=model.tags.all() #Fetch all tags for edit form
+    obj=model.objects.get(slug=slug) #Fetch the article being demanded
+    mode="display" # Mode is "edit" when the user wished to edit any article
     if request.method=="GET" and 'edit' in request.GET:
         edit=request.GET['edit']
-        edit_field=edit.split(',')
-        request.session['edit_field']=edit_field
+        edit_field=edit.split(',')#split fieds user wished to edit. One user may want to edit more than 1 field at once
+        request.session['edit_field']=edit_field#Store fields in session
         request.session.modified = True
-        form= PostForm(model,edit_field,'edit',instance=obj)
-        mode="edit"
+        form= PostForm(model,edit_field,'edit',instance=obj)#Fetch edit form
+        mode="edit" #Change mode
     elif request.method=="POST":
         form= PostForm(model,request.session['edit_field'],'edit',request.POST,instance=obj)
         mode="edit"
-        if form.is_valid():
-            instance=form.save(commit=False)
-            user=get_user(request)
-            instance.save()
+        if form.is_valid():#If edit is valid
+            instance=form.save(commit=False)#Commit to DB
+            user=get_user(request)#Get  user
+            instance.save()#Save instance
             form.save_m2m()
-            automoderate(instance,user)
+            automoderate(instance,user)#Send to moderation
             return HttpResponseRedirect(reverse('single',kwargs={'section':section,'slug':obj.slug}))
     else:
-        form=None
+        form=None#If no edit is there, just display the article
     recent=model.objects.all().filter(state="submitted").order_by('-published')[:5]
     return render(request,'single.html',{'obj':obj,'section':section,'full_url':request.build_absolute_uri(),"mode":mode,"form":form,"tags":tags,'page':'single','recent':recent})
 
-
+"""
+Method for upvoting/downvoting articles
+"""
 def vote(request,section,choice,slug):
-    model=get_model(section)
-    obj=model.objects.get(slug=slug)
+    model=get_model(section)#Identify the type of article - Blogs,Tutorials,etc
+    obj=model.objects.get(slug=slug)#get slug for article to vote
     v=model.objects.from_request(request).get(pk=obj.pk)
-    token=request.secretballot_token
+    token=request.secretballot_token#Get unique hash for each IPAddress so that users can vote only once
     if(choice=='upvote'):
         t=1
     else:
         t=-1
     if v.user_vote==t:
-        obj.remove_vote(token)
+        obj.remove_vote(token)#Voting the same article twice removes the vote
     else:
         obj.add_vote(token,t)
     return HttpResponseRedirect(reverse('single',kwargs={'slug':slug,'section':section}))
@@ -119,8 +143,8 @@ General listing of all sections
 """
 
 def all(request,section,**kwargs):
-    model=get_model(section)
-    name=get_name(section)
+    model=get_model(section)#Identify the type of article - Blogs,Tutorials,etc
+    name=get_name(section)# Get generic name
     """
     if(model==Tutorial or model==EducationalResource or model==Wiki or model==Snippet):
         s_area="tl"
@@ -134,10 +158,11 @@ def all(request,section,**kwargs):
     s=None
     f=None
     message=""
-    if 'sort' in request.GET:
+    if 'sort' in request.GET:#If any sort parameters exist
         sort=request.GET['sort']
-        s=sort
+        s=sort#Store sort parameters in s
         message+="Ordered by "+sort+"   "
+        #Sort according to the user's choice
         if sort=="ratings":
             obj_list=model.objects.all().filter(state="submitted").order_by('-total_upvotes')
         elif sort=="alphabetical":
@@ -145,11 +170,11 @@ def all(request,section,**kwargs):
         else:
             obj_list=model.objects.all().filter(state="submitted").order_by('-published')
     else:
-        if section=="packages":
+        if section=="packages": #Default way to display packages is alphabetically
              obj_list=model.objects.all().filter(state="submitted").order_by(Lower('title'))
         else:
             obj_list=model.objects.all().filter(state="submitted").order_by('-published')
-    if 'tags' in request.GET:
+    if 'tags' in request.GET:#If filtering by tags is required
         tags=request.GET['tags']
         t=tags
         if message=="":
